8e7e38cb6774140378b0e8077f23e20439cbcc62
Anka <runner@sat12-bq161-49816a08-76a4-4ae5-b04a-17024f086aa6-924D211E3329.local>
2025-09-30 07:45:57 +0000

    chore(release): update SPM definition

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://sdk-assets.realtime.cloudflare.com/RealtimeKit-1.5.0-14-g7fe346799d-a080a58e-3c36-4d58-acd7-a5f008482260.xcframework.zip"
+let remoteKotlinChecksum = "5055045232a7b19d74adca2bdaa598969018bd0d568e19b94ee7226daa9dbf27"
+let packageName = "RealtimeKit"
+// END KMMBRIDGE BLOCK
+
+let package = Package(
+    name: "RealtimeKit",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: packageName, targets: [packageName, "RTKWebRTC"]),
+        .library(name: "RTKWebRTC", targets: ["RTKWebRTC"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "RTKWebRTC",
+            url: "https://sdk-assets.realtime.cloudflare.com/RTKWebRTC-v125.6422.07.zip",
+            checksum: "114cb3ea15c5709f2c35d2b1c7a64e742a6902d375d54895984263bb79d75ce3"
+        ),
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        ),
+    ]
+)
