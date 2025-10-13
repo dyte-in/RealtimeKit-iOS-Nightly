28929c4d8a54933e76cced606589527770511c3c
Anka <runner@sat12-bq158-78312c21-024e-4429-928e-af545b4b3b1a-16D40BB310CC.local>
2025-10-13 11:49:39 +0000

    chore(release): update SPM definition

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://sdk-assets.realtime.cloudflare.com/RealtimeKit-1.5.1-13-g689f7bf595-7044c2a4-8ad2-435d-bac5-5ddf2791b93b.xcframework.zip"
-let remoteKotlinChecksum = "65616476a97462990cc47ddf5282712343ad0fec9bfb10ddb0bb6b88efe934f1"
+let remoteKotlinUrl = "https://sdk-assets.realtime.cloudflare.com/RealtimeKit-1.5.1-18-gb7ddfa7540-671f5141-df70-4863-9759-400143c8bdf9.xcframework.zip"
+let remoteKotlinChecksum = "6ea5593dd1bc15a9f3fa9106d7b77e38505b0b40e27c307f6f79477bf1325e66"
 let packageName = "RealtimeKit"
 // END KMMBRIDGE BLOCK
 
